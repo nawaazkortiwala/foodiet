70bda7374e39f0185636633ed74887518ad4a950
nawaazwill11 <nawaazkortiwala@gmail.com>
2021-12-12 07:27:31 +0530

    added and configured .env file

--- server/dist/db.js
+++ server/dist/db.js
@@ -1,12 +1,14 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const pg_1 = require("pg");
+const dotenv_1 = require("dotenv");
+(0, dotenv_1.config)();
 const pool = new pg_1.Pool({
-    user: 'pos-dev',
-    database: 'foodiet',
-    password: '1q2w3e4r5t6y',
-    host: '139.59.84.5',
-    port: 5432,
+    user: process.env.DB_USER,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASS,
+    host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT),
 });
 exports.default = pool;
 //# sourceMappingURL=db.js.map
--- server/src/db.ts
+++ server/src/db.ts
@@ -1,11 +1,14 @@
 import { Pool } from 'pg'
+import { config } from 'dotenv'
+
+config()
 
 const pool = new Pool({
-    user: 'pos-dev',
-    database: 'foodiet',
-    password: '1q2w3e4r5t6y',
-    host: '139.59.84.5',
-    port: 5432,
+    user: process.env.DB_USER,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASS,
+    host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT),
 })
 
 export default pool
